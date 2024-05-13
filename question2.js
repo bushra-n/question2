74b15f9fc701b95ec9d7fa82a4ff921bec3414c2
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-13 17:05:54 +0500

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,3 @@
+// Store a person name in variable then print a message to that person.
+var firstName = "Eric";
+console.log("Hello ".concat(firstName, ",would you like to learn some python today?"));
---
+++ question2.ts
@@ -0,0 +1,8 @@
+// Store a person name in variable then print a message to that person.
+let firstName:string="Eric"
+console.log(`Hello ${firstName},would you like to learn some python today?`)
+
+
+
+
+
